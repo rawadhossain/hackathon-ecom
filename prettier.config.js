11afba52a155d8e62841457146fbea238c5255c7
Rawad Hossain <rawad.hossain00@gmail.com>
2025-05-24 10:22:43 +0600

    prettier config

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	tabWidth: 4, // Sets tab width to 4 spaces
+	useTabs: true, // Uses tabs instead of spaces
+	semi: true, // Adds semicolons at the end of statements
+	singleQuote: true, // Enforces single quotes instead of double quotes
+	bracketSpacing: true,
+	trailingComma: 'es5',
+	printWidth: 100,
+};
