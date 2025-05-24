3ab67797b024b9a809343f1a9f98f917fe2a3bd6
Kazi Badrul <kazibadrul03@gmail.com>
2025-05-24 22:09:14 +0600

    Added Button on Dashboard

--- src/app/(main)/dashboard/page.jsx
+++ src/app/(main)/dashboard/page.jsx
@@ -4,7 +4,16 @@ import Link from 'next/link';
 import React from 'react';
 
 const page = () => {
-	return <div></div>;
+	return (
+		<div className="mt-5 mx-2">
+			<Link
+				href="/listings"
+				className="px-2 py-3 bg-foregroundCustom text-white border rounded-xl"
+			>
+				View Listings
+			</Link>
+		</div>
+	);
 };
 
 export default page;
